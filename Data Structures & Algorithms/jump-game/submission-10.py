4b238e71cf696dec6f1452eacb7eb64f45e300a8
KaiyueWei <kaiyue.wei@outlook.com>
2026-04-22 00:16:43 +0000

    Add: jump-game - submission-10

---
+++ Data Structures & Algorithms/jump-game/submission-10.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [False] * len(nums)
+        dp[n-1] = True
+
+        for i in range(n-2, -1, -1):
+            end = min(i + nums[i] + 1,n)
+            for j in range(i+1, end):
+                if dp[j]:
+                    dp[i] = True
+                    break
+        return dp[0]
+        
